6146dbda1883b4e6681010ffed9194890b758176
Matheu Plouffe <matheu.plouffe@ubisoft.com>
2020-02-14 11:34:01 -0600

    Project complete.

--- public/js/index.js
+++ public/js/index.js
@@ -1,105 +1,39 @@
-// create scene
-var graphicsContainer = {
-    scene: null,
-    sceneObjects: {},
-    renderer: null,
-    camera: null,
-    statsContainer: {
-        highestFitnessScore: 0,
-        averageFitness: 0
-    },
-    statsDiv: null,
-    statusDiv: null
-};
-
-var worldObjects = {
-    world: null,
-    playerOne: null
-}
-
-var population;
-var frame = 1;
-var timeStep = 1/60;
-
-window.addEventListener('DOMContentLoaded', (event) => {
-    // graphics
-    initGraphics(graphicsContainer);
-    initCannon(worldObjects);
-    graphicsContainer.statsDiv = document.getElementById("stats");
-    graphicsContainer.statusDiv = document.getElementById("status");
-
-    // genetics
-    worldObjects.playerOne = new Ship(new CANNON.Vec3(0,0,0), graphicsContainer, worldObjects.world);
-
-    animate();
-
-    document.addEventListener('keydown', keyPressed);
-});
-
-window.addEventListener('resize', (event) => {
-    graphicsContainer.camera.aspect = window.innerWidth / window.innerHeight;
-    graphicsContainer.camera.updateProjectionMatrix();
-    graphicsContainer.renderer.setSize(window.innerWidth, window.innerHeight);
-    updateSanityCube(graphicsContainer.camera);
-});
-
-function keyPressed(e) {
-    console.log(e.code);
-    switch (e.code) {
-        case "KeyW":
-            worldObjects.playerOne.thrust();
-            break;
-        case "KeyA":
-            worldObjects.playerOne.rotateCC();
-            break;
-        case "KeyD":
-            worldObjects.playerOne.rotateC();
+function addElem(numElements, elementId) {
+    for (let i = 0; i < numElements; i++) {
+        let elm = document.createElement("img");
+        elm.src = "images/invader.gif";
+        elm.setAttribute("class", "invader");
+        document.getElementById(elementId).appendChild(elm);
     }
 }
 
-function animate() {
-    // if (frame % population.generationLifetime == 0) {
-    //     frame = 1;
-    //     population.fitness(target);
-    //     population.selection();
-    //     population.reproduction();
-    //     // population.resetPopulation();
-    // } else {
-    //     frame++;
-    //     population.update();
-    //     population.checkCollisions(obstacles);
-    // }
+var lines = [2, 4, 9, 9, 7, 5, 3, 1 ]
+var ids = [
+    ["line_01_A", "line_01_B"],
+    ["line_02_A", "line_02_B"],
+    "line_03",
+    "line_04",
+    "line_05",
+    "line_06",
+    "line_07",
+    "line_08"
+]
 
-    updateStatsDisplay();
-    updateStatusDisplay();
+function buildElem() {
+    lines.forEach((value, index) => {
+        if (index < 2) {
+            let elemId = ids[index][0];
+            addElem(value, elemId);
 
-    // update world physics
-    worldObjects.world.step(timeStep);
-
-    // update player
-    worldObjects.playerOne.update();
-
-    // animate sanity cube
-    graphicsContainer.sceneObjects.sanityCube.rotation.x += 0.01;
-    graphicsContainer.sceneObjects.sanityCube.rotation.y += 0.01;
-    graphicsContainer.renderer.render(graphicsContainer.scene, graphicsContainer.camera);
-    requestAnimationFrame( animate );
+            elemId = ids[index][1];
+            addElem(value, elemId);
+        } else {
+            let elemId = ids[index];
+            addElem(value, elemId);
+        }
+    });
 }
 
-function updateStatsDisplay() {
-    let statsString = "Stats";
-
-    graphicsContainer.statsDiv.innerHTML = statsString;
-}
-
-function updateStatusDisplay() {
-    let statusString = "Status"
-    graphicsContainer.statusDiv.innerHTML = statusString;
-}
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    let value = Math.floor(Math.random() * (max - min + 1)) + min;
-    return value === 64 ? 32 : value;
-}
+window.addEventListener('DOMContentLoaded', (event) => {
+    buildElem();
+});
